52993f686ea21e8bd0488edd76c215025f6216fc
Sai Sashreek <saisashreekd@gmail.com>
2024-02-09 12:41:19 +0530

    Page Links Updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,21 @@
+import LoginHeader from "./forms/LoginHeader";
+import LoginForm from "../components/forms/LoginForm";
+
 export default function Login () {
     return (
-        <></>
+        <>
+            <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-md w-full space-y-8">
+                    <LoginHeader
+                        heading="Login to your account"
+                        paragraph="Don't have an account yet? "
+                        linkName="Signup"
+                        linkUrl="/register" 
+                    />
+
+                    <LoginForm />
+                </div>
+            </div>
+        </>
     );
 }
