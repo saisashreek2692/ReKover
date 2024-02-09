40aadbf65f9c4b5c1de20434747cbe5d715d7eba
Sai Sashreek <saisashreekd@gmail.com>
2024-02-09 11:37:42 +0530

    Banner Section Updated

--- src/pages/sections/Banner.jsx
+++ src/pages/sections/Banner.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function Banner() {
   return (
     <>
@@ -17,11 +19,11 @@ export default function Banner() {
                   convey your message and tell your story.
                 </p>
                 <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                  <a
-                    href="#_"
+                  <Link
+                    to="/login"
                     className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto"
                   >
-                    Try It Free
+                    Get Started
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-5 h-5 ml-1"
@@ -35,13 +37,13 @@ export default function Banner() {
                       <line x1="5" y1="12" x2="19" y2="12"></line>
                       <polyline points="12 5 19 12 12 19"></polyline>
                     </svg>
-                  </a>
-                  <a
-                    href="#_"
+                  </Link>
+                  <Link
+                    to="/"
                     className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600"
                   >
                     Learn More
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
