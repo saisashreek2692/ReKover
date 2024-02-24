6a5a70d85bf7dfa3df5b3a53b5ead83f4db8646c
Sai Sashreek <saisashreekd@gmail.com>
2024-02-24 12:09:27 +0530

    New Dashboard Page Created

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="grid grid-rows-2 grid-flow-col gap-4">
+      <div className="row-span-1 bg-teal-500 w-16">01</div>
+      <div className="row-span-2 bg-cyan-500 h-10">02</div>
+    </div>
+  );
+};
+
+export default Dashboard;
