70b30b741d45d51a6789a2c38cac988d7829bedd
Sai Sashreek <saisashreekd@gmail.com>
2024-02-26 18:19:34 +0530

    Banner SCSS Added

--- src/pages/sections/Banner.jsx
+++ src/pages/sections/Banner.jsx
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <>
-      <section className="px-2 py-32 bg-white md:px-0">
-        <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-          <div className="flex flex-wrap items-center sm:-mx-3">
-            <div className="w-full md:w-1/2 md:px-3">
-              <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                  <span className="block xl:inline">Useful Tools to</span>
-                  <span className="block text-indigo-600 xl:inline">
+      <section className="banner__BG">
+        <div className="banner__Container">
+          <div className="banner__Container--Flex">
+            <div className="banner__Section-1">
+              <div className="banner__Section--Left">
+                <h1 className="banner__Section--Heading">
+                  <span className="banner__Section--Heading-Primary">Useful Tools to</span>
+                  <span className="banner__Section--Heading-Secondary">
                     Help You Build Faster.
                   </span>
                 </h1>
-                <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
+                <p className="banner__Section--Desc">
                   It&apos;s never been easier to build beautiful websites that
                   convey your message and tell your story.
                 </p>
-                <div className="relative flex flex-col sm:flex-row sm:space-x-4">
+                <div className="banner__Section--Btns-Flex">
                   <Link
                     to="/login"
-                    className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto"
+                    className="banner__Section--Btns-1"
                   >
                     Get Started
                     <svg
@@ -41,15 +41,15 @@ const Banner = () => {
                   </Link>
                   <Link
                     to="/"
-                    className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600"
+                    className="banner__Section--Btns-2"
                   >
                     Learn More
                   </Link>
                 </div>
               </div>
             </div>
-            <div className="w-full md:w-1/2">
-              <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+            <div className="banner__Section-2">
+              <div className="banner__Section--Right">
                 <img src="https://images.unsplash.com/photo-1498049860654-af1a5c566876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
               </div>
             </div>
