a120fbb7bc2b78b70a38d6b7f4ad0a1468d0409a
Sai Sashreek <saisashreekd@gmail.com>
2024-02-26 16:47:52 +0530

    Page Imports Updated

--- src/pages/sections/index.js
+++ src/pages/sections/index.js
@@ -1,4 +1,6 @@
 export { default as Header } from "./Header";
+export { default as Headerform } from "./Headerform";
+export { default as Headernav } from "./Headernav";
 export { default as Banner } from "./Banner";
 export { default as About } from "./About";
 export { default as AboutProd } from "./AboutProd";
